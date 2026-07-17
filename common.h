97dba09c51cd9d6790ecee398711a48f25c93c2b
StarlinkLexus <ale_vallejo16@outlook.es>
2026-07-17 09:14:43 -0500

    Cambio en estructuras

--- common.h
+++ common.h
@@ -4,25 +4,27 @@
 
 #include <sys/types.h>
 #include <time.h>
+#include <semaphore.h> // Asegúrate de incluir esto
 
 #define MAX_PATH 1024
 #define MAX_FILES 500
 #define SHM_NAME "/shm_sync_stats"
-#define SEM_NAME "/sem_sync_stats"
 #define FIFO_NAME "/tmp/fifo_sync_logger"
 
-// Estructura de estadísticas en memoria compartida (Requerimiento)
+// Estructura de estadisticas en memoria compartida con semáforo integrado
 struct stats {
+    sem_t sem_control; // Semáforo basado en memoria
     long archivos_copiados;
     long bytes_copiados;
     long errores;
 };
 
-// Estructura para almacenar metadatos de los archivos en memoria
 typedef struct {
     char ruta_relativa[MAX_PATH];
-    off_t tamano;
-    time_t mtime;
+    off_t tamano;        
+    time_t mtime;        
+    ino_t inode;         
+    mode_t permisos;     
 } ArchivoMetadata;
 
 #endif
